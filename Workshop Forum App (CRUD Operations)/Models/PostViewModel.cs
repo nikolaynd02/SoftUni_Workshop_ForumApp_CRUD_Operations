78686ef317c0f15f261c60ee17c8130d91f095dd
Nikolay Dobrev <nickndobrev@gmail.com>
2022-10-05 19:42:24 +0300

    models, props and views

---
+++ Workshop Forum App (CRUD Operations)/Models/PostViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Workshop_Forum_App__CRUD_Operations_.Models
+{
+    public class PostViewModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+    }
+}
